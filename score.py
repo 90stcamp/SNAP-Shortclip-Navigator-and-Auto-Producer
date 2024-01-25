ddc4eefe732d49b865b76102e6200c708bc26073
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-01-25 12:53:19 +0900

    delete: move to utils

--- score.py
+++
@@ -1,7 +0,0 @@
-from rouge import Rouge
-
-
-def get_Rouge_score(list_generated,list_abstract):
-    rouge = Rouge()
-    dict_score=rouge.get_scores(list_generated,list_abstract,avg=True)
-    return dict_score
