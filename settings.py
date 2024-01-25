ae0638ac783b5f0928a5ee5a87f46c8218772acf
in-sukim <rladlstn494@naver.com>
2024-01-25 09:58:10 +0000

    update settings.py

--- settings.py
+++ settings.py
@@ -3,11 +3,11 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 CONFIG_DIR = os.path.join(BASE_DIR, 'config.json')
 API_KEY = os.path.join(BASE_DIR, 'key.json')
-# OUT_DIR = os.path.join(BASE_DIR, 'results')
+OUT_DIR = os.path.join(BASE_DIR, 'results')
 # FIG_DIR = os.path.join(BASE_DIR, 'asset')
 
-# if not os.path.exists(PARAM_DIR):
-#     os.mkdir(PARAM_DIR)
+if not os.path.exists(OUT_DIR):
+    os.mkdir(OUT_DIR)
 
 # if not os.path.exists(OUT_DIR):
 #     os.mkdir(OUT_DIR)
