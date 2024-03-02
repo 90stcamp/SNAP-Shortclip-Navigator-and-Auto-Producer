04e760e44a1134d623b3bd79f82acc6dca2480a6
jaealways <jhyj121000@gmail.com>
2024-03-02 10:59:17 +0900

    init: heatmap crawling

--- crawlView.py
+++ crawlView.py
@@ -2,27 +2,32 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(options=options)
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
 
+css_heat_map_path = "ytp-heat-map-path"
 
-driver.get("https://www.youtube.com/watch?v=VHre-G8wlb4")
+def get_html(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    bar=driver.find_element(By.CSS_SELECTOR, "div.ytp-progress-bar-container")
+    action = ActionChains(driver)
+    # perform the operation
+    action.move_to_element(bar).click().perform()
+    bar.click()
+
+    # Get the page source once the element is available
+    graph=driver.find_element(By.XPATH, "svg/defs/clipPath")
+
+    # Extract the 'd' attribute from each selected element
+    d_values = [element.get('d') for element in graph]
+    return d_values
 
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-progress_bar = soup.find("div", {"id": "progress-bar-id"})
-
-if progress_bar:
-    viewed_percentage = progress_bar.text
-
-driver.quit()
-
-print(viewed_percentage)
+# Example usage:
+heat_map_data = get_html("https://www.youtube.com/watch?v=279KL-eVnVM")
+print(heat_map_data)
