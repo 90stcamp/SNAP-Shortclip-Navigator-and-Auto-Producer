e17633e73d85bfb4e5a11017bc30cd56dc0ee489
jaealways <jhyj121000@gmail.com>
2024-03-03 16:39:45 +0900

    feat: add text retrieval logic

--- utils/scores.py
+++ utils/scores.py
@@ -3,6 +3,8 @@ import pandas as pd
 from settings import *
 import numpy as np
 from tqdm import tqdm
+from rouge_score import rouge_scorer
+
 
 def get_Rouge_score(list_generated,list_abstract):
     rouge = Rouge()
@@ -38,8 +40,6 @@ def statistic_from_rouge_list(result_name):
     print(f"Mean :{mean}")
     print(f"Standard Deviation:{std}")
 
-
-
 def save_rouge_avg(avg_array, save_name):
     result_dic = {'model_name':save_name, 'values':avg_array}
     if not os.path.exists(os.path.join(STATS_DIR, f"{save_name}_result.npy")):
@@ -47,3 +47,32 @@ def save_rouge_avg(avg_array, save_name):
     else:
         print('기존 파일이 존재합니다. 다른 save_name을 설정해주세요.')
         raise
+
+def top_k_text(text, shorts_time =60,k = 3 ,interval = 1):
+    candidates = [] #쇼츠 후보군들
+    # shorts_time = 60 #쇼츠후보들의 초수
+    # k = 3            #몇개의 후보를 선정할건지
+    # interval = 1     #몇개의 문장을 기준으로 나눌껀지
+
+    for i in range(0,len(text[0]),interval):
+        start = text[0][i][1]       #시작시간
+        temptext = text[0][i][0]    #첫문장
+        for j in range(i+1,len(text[0])):
+            end = text[0][j][2]     #끝나는시간
+            temptext= ' '.join([temptext,text[0][j][0]])
+            if end-start>shorts_time:
+                break               #계속 합치다가 60초가 넘어가면 break
+        candidates.append([temptext,start,end]) #60초동안의 문장,시작시간,끝나는시간 저장
+        if j == len(text[0])-1:     #끝나는시간이 영상끝이라면 끝
+            break
+        
+    scorer = rouge_scorer.RougeScorer(['rouge1'], use_stemmer=True)
+    for i, candidate in enumerate(candidates):
+        scores = scorer.score(text[1], candidate[0])    #우리가 요약한 문장과 쇼츠 후보와 rouge1점수 비교
+        candidates[i] = [*candidate,scores['rouge1'][2]]#[60초동안의 문장, 시작시간,끝나는시간, rouge1_F1점수]
+
+    candidates.sort(reverse = True, key = lambda x : x[3])#rouge1_F1점수로 sort
+
+    final_candidates = candidates[:k]
+    return final_candidates
+
