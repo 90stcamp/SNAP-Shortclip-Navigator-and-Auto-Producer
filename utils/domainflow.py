967cc5618817e4987861408ae8775fffe6936008
jaealways <jhyj121000@gmail.com>
2024-03-05 23:18:43 +0900

    feat: add reduce flow

--- utils/domainflow.py
+++ utils/domainflow.py
@@ -9,11 +9,16 @@ def domain_entertainment(text):
 def domain_comedy(text):
     pick=summarize_langchain({"document": text}, prompts.prompt_comedy_pick())['text']
     output=summarize_langchain({"document": text, "pick":pick }, prompts.prompt_retrieval())
-    print(output)
     return output['text']
 
 def domain_sports(text):
-    output=summarize_langchain(text, prompts.prompt_domain_summ_entertain())
+    pick=summarize_langchain(text, prompts.prompt_sports_pick())['text']
+    output=summarize_langchain({"document": text, "pick":pick }, prompts.prompt_retrieval())
+    return output['text']
+
+def domain_game(text):
+    pick=summarize_langchain(text, prompts.prompt_game_pick())['text']
+    output=summarize_langchain({"document": text, "pick":pick }, prompts.prompt_retrieval())
     return output['text']
 
 def separate_to_domain(domain, text):
@@ -21,5 +26,12 @@ def separate_to_domain(domain, text):
         return domain_entertainment(text)
     elif domain=="Sports":
         return domain_sports(text)
-    elif domain=="Comdey":
+    elif domain=="Comedy":
         return domain_comedy(text)
+    elif domain=="Gaming":
+        return domain_game(text)
+
+def separate_reduce_domain(domain, text):
+    if domain=="Entertainment":
+        prompt=prompts.prompt_reduce_entertain_pick()
+    return summarize_mapreduce(text, prompt)
