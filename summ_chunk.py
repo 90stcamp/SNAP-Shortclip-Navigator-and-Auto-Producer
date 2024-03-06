9f88986809f2e545044e5710cc44c5f19b6477e7
in-sukim <rladlstn494@naver.com>
2024-03-06 11:35:40 +0900

    feat async chunk mapreduce

--- summ_chunk.py
+++ summ_chunk.py
@@ -22,6 +22,7 @@ config = utils.load_json(CONFIG_DIR)
 np.random.seed(2024)
 
 from concurrent.futures import ThreadPoolExecutor, as_completed, ProcessPoolExecutor
+import asyncio
 
 @contextmanager
 def timer(name):
@@ -36,11 +37,29 @@ def enter_reduce_prompt():
     """
     return template
 
-def process_chunk(chunk):
-    # 청크를 map_chain을 통해 처리하는 코드로 대체
-    formatted_input = {"document": chunk.page_content}  # 'content'는 청크 내용을 가리키는 키
-    result = map_chain.apply([formatted_input])  # 가정: map_chain.apply 메소드 사용
+# def process_chunk(chunk):
+#     # 청크를 map_chain을 통해 처리하는 코드로 대체
+#     formatted_input = {"document": chunk.page_content}  # 'content'는 청크 내용을 가리키는 키
+#     result = map_chain.apply([formatted_input])  # 가정: map_chain.apply 메소드 사용
+#     return result
+
+executor = ThreadPoolExecutor(max_workers=2)
+
+async def process_chunk_async(chunk, executor):
+    loop = asyncio.get_running_loop()
+    # 주어진 입력으로 formatted_input을 구성합니다.
+    formatted_input = {"document": chunk.page_content}
+    # ThreadPoolExecutor를 사용하여 map_chain.apply를 비동기적으로 실행합니다.
+    result = await loop.run_in_executor(executor, lambda: map_chain.apply([formatted_input]))
     return result
+
+async def map_async(chunks, executor):
+    # 비동기 작업을 준비합니다.
+    tasks = [process_chunk_async(chunk, executor) for chunk in chunks]
+    
+    # 모든 비동기 작업을 실행하고 완료될 때까지 기다립니다.
+    results = await asyncio.gather(*tasks)
+    return results
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -57,7 +76,7 @@ if __name__=='__main__':
         '--chunk_size', default=1024, type=int
         )
     parser.add_argument(
-        '--chunk_overlap', default=200, type=int
+        '--chunk_overlap', default=100, type=int
         )
     parser.add_argument(
         '--save_name', required=True, type=str
@@ -66,30 +85,39 @@ if __name__=='__main__':
 
     MODEL_NAME = config['model_name'][args.model_num]
     cache_dir = config['cache_dir'][args.model_num]
-    with timer('Summarization'):
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME,cache_dir=cache_dir)
-        tokenizer.pad_token_id = tokenizer.eos_token_id
-        model = AutoModelForCausalLM.from_pretrained(
-            MODEL_NAME,
-            cache_dir=cache_dir)
-        pipe = pipeline(
-            "text-generation", 
-            model=model, 
-            tokenizer=tokenizer, 
-            max_new_tokens= args.max_token_num,
-            device = 0, 
-            pad_token_id=tokenizer.eos_token_id)
-        hf = HuggingFacePipeline(pipeline=pipe)
-        torch.cuda.empty_cache()
 
+    print("Model, pipeline Setting Start...")
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_NAME,
+        cache_dir=cache_dir)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME,
+        cache_dir=cache_dir)
+    pipe = pipeline(
+        "text-generation", 
+        model=model, 
+        tokenizer=tokenizer, 
+        max_new_tokens= args.max_token_num,
+        device = 0, 
+        pad_token_id=tokenizer.eos_token_id)
+    
+    hf = HuggingFacePipeline(pipeline=pipe)
+    torch.cuda.empty_cache()
+    print("Model, pipeline Setting End...")
+    
+    with timer("Map Chain"):
+        print("Map Chain Start")
         map_template = PromptTemplate(
-            template=prompts.prompt_entertaining_map(),
+            template=prompts.prompt_extsum_paper2(),
             input_variables=["document"]
             )
         map_chain = LLMChain(llm=hf, prompt=map_template)
         torch.cuda.empty_cache()
 
         text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            # tokenizer = tokenizer,
             separator= '\n',
             chunk_size = 1024,
             chunk_overlap = 100,
@@ -102,23 +130,31 @@ if __name__=='__main__':
         split_docs = text_splitter.create_documents([text])
 
         chunks = split_docs  # split_docs는 텍스트를 청크로 나눈 결과 리스트
-        max_workers = os.cpu_count()
 
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            futures = [executor.submit(process_chunk, chunk) for chunk in chunks]
-            # 완료된 작업의 결과 처리
-            results = []
-            for future in as_completed(futures):
-                results.append(future.result())
+        # with ThreadPoolExecutor(max_workers=2) as executor:
+        #     futures = [executor.submit(process_chunk, chunk) for chunk in chunks]
+        #     # 완료된 작업의 결과 처리
+        #     results = []
+        #     for future in as_completed(futures):
+        #         torch.cuda.empty_cache()
+        #         # print(len(tokenizer.tokenize(future.result()[0]['text'])))
+        #         results.append(future.result())
+        map_result = asyncio.run(map_async(chunks, executor))
 
+    with timer("Reduce Chain"):
         reduce_template = PromptTemplate(
             template=prompts.prompt_entertaining_reduce(),
             input_variables=["document"]
             )
+        torch.cuda.empty_cache()
         reduce_chain = LLMChain(llm=hf, prompt=reduce_template) 
 
-        str_list = [result[0]['text'] for result in results]
-        reduce_result = reduce_chain.invoke(' '.join(str_list))['text']
+        str_list = [result[0]['text'] for result in map_result]
+        reduce_target = ' '.join(str_list)
+        print(len(tokenizer.tokenize(reduce_target)))
+
+        ## 긴 영상에 대해 어떻게 처리할지(MLB 3:08:58 기준: 6649 토큰 -> CUDA Error)
+        # reduce_result = reduce_chain.invoke(reduce_target)['text']
 
         with open(os.path.join(SUMM_DIR, f"result/{args.save_name}.txt"), 'w') as f:
-            f.write(reduce_result)
+            f.write(reduce_target)
