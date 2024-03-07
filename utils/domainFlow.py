311b0ec14a0158b36fe56f75fa5b8f6e693ce7b2
JAE-HYEONG LEE <71856506+jaealways@users.noreply.github.com>
2024-03-07 14:56:27 +0900

    feat: update prompts

--- utils/domainFlow.py
+++ utils/domainFlow.py
@@ -34,4 +34,14 @@ def separate_to_domain(domain, text):
 def separate_reduce_domain(domain, text):
     if domain=="Entertainment":
         prompt=prompts.prompt_reduce_entertain_pick()
+    elif domain=="News \\u0026 Politics":
+        prompt=prompts.prompt_reduce_news_pick()
+    elif domain=="Comedy":
+        prompt=prompts.prompt_reduce_comedy_pick()
+    elif domain=="Science \\u0026 Technology":
+        prompt=prompts.prompt_reduce_sci_pick()
+    elif domain=="Travel \\u0026 Events":
+        prompt=prompts.prompt_reduce_travel_pick()
+    else:
+        raise Exception('domain does not match')
     return summarize_mapreduce(text, prompt)
