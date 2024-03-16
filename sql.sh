b58c88421ce68ec96986bf9b5fb30e6565734891
in-sukim <rladlstn494@naver.com>
2024-03-16 21:47:33 +0900

    feat: while server loading

--- sql.sh
+++ sql.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # python3 'server2.py'
-docker-compose up
-docker-compose down
-docker volume rm youtube-short-generator_shared-data
+# docker-compose up
+# docker-compose down
+# docker volume rm youtube-short-generator_shared-data
 
-# while true; do
-    # python3 'server2.py'
-    # docker-compose up
-    # docker-compose down
-    # docker volume rm  kis_shared-data
-#     sleep 1  
-# done
+while true; do
+    python3 'server2.py'
+    docker-compose up
+    docker-compose down
+    docker volume rm  youtube-short-generator_shared-data
+    sleep 1  
+done
