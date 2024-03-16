b5ded97b0e14d35c87dfe592f301df547055395e
in-sukim <rladlstn494@naver.com>
2024-03-16 16:19:02 +0900

    update: volume name

--- sql.sh
+++ sql.sh
@@ -3,7 +3,7 @@
 # python3 'server2.py'
 docker-compose up
 docker-compose down
-docker volume rm  kis_shared-data
+docker volume rm youtube-short-generator_shared-data
 
 # while true; do
     # python3 'server2.py'
